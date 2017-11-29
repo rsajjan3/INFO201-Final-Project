0320f5f73430277e83a05fb5ca7a1533aa36b58c
tuckemeg000 <tuckemeg000@gmail.com>
2017-11-28 17:13:59 -0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,1 +1,25 @@
 # ui.R file that defines how the application will be displayed in the browser
+# Created by Megan Tucker
+library("shiny")
+library("dplyr")
+library("markdown")
+
+
+shinyUI(navbarPage("Natality in Washington State",
+                   tabPanel("About"
+                            # Print paragraph regarding this project
+                            # Print paragraph regarding the dataset
+                            ),
+                   tabPanel("Histogram"
+                            # Call method to print histogram
+                            ## Check boxes widget to mainpulate displayed races
+                            ),
+                   tabPanel("Map"
+                            # Call menthod to print map
+                            ## Range slider widget to mainuplate number of birth min/max
+                            ),
+                   tabPanel("Plot"
+                            # Call method to print plot
+                            ## Check boxes widget to manipulate ______
+                            )
+))

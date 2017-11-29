33fb45c6d9b0beed3973551d3399eb36999701b3
Ravi Sajjan <rsajjan3@uw.edu>
2017-11-28 17:15:53 -0800

    Wrote code for histogram

--- histogram.R
+++ histogram.R
@@ -1,2 +1,8 @@
 # Create a histogram for given data
 # Seungju Choi
+library(dplyr)
+natality.data <- read.csv('data.csv')
+RaceDistrubtion <- function(data) {
+  wrangled.data <- filter(data, County =='King County, WA') %>% group_by(Race) %>% summarise(births=sum(Births))
+  return(wrangled.data)
+}
--- plot.R
+++ plot.R
@@ -2,5 +2,5 @@ library(dplyr)
 natality.data <- read.csv('data.csv')
 AgeTrend <- function(data) {
   wrangled.data <- group_by(data, Year, Age.of.Mother.Year) %>% summarise(births=sum(Births))
-  View(wrangled.data)
+  return(wrangled.data)
 }

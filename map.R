adf10aa61a54c00956ba5f92b319a80832f9890e
Hsien-Cheng Chou <jchou3@uw.edu>
2017-12-03 19:29:22 -0800

    create map.R but has some issues

--- map.R
+++ map.R
@@ -1,31 +1,51 @@
 # Create map.R file 
 # Jason Chou
-install.packages('plotly')
 library(dplyr)
 library(plotly)
 library(ggplot2)
-# Create Maps
-devtools::install_github('ropensci/plotly')
+library(tidyverse)
+library(maps)
 
-# geo styling
-geo.style <- list(scope = 'Washington State', 
-                  projection = list(type = 'albers usa'), 
-                  showland = TRUE, landcolor = toRGB("gray95"), 
-                  subunitcolor = toRGB("gray85"), 
-                  countrycolor = toRGB("gray85"), 
-                  countrywidth = 0.5, 
-                  subunitwidth = 0.5)
-
-# Structuring plot for the shootings
-plot <- plot_geo(natality.data, lat = ~lat, lon= ~lng) %>%
-  add_markers(
-    text = ~paste(paste("(", lng, ",", lat, ")"), paste(city, ",", state), paste("People Injured:", injured), paste("People Killed:", killed), sep = "<br />"),
-    color = ~injured, colors = c("lightblue", "purple"), size = ~killed, hoverinfo = "text") %>%
-  layout(title = "2017 United States shootings<br />(Hover for more info)",geo = geo.style)
-
-library(dplyr)
 natality.data <- read.csv('data.csv')
 BirthsPerCounty <- function(data) {
   wrangled.data <- group_by(data, County) %>% summarise(births=sum(Births))
   return(wrangled.data)
 }
+
+wash <- map_data("county") %>%
+  filter(region == 'washington')
+
+births <- natality.data %>%
+  group_by(County) %>%
+  summarise(Births = sum(Births))
+
+births$County <- tolower(gsub(" County, WA","", births$County))
+
+births <-filter(births, County %in% c("benton", "clark", "cowlitz", "king", "kitsap", "pierce", "skagit", "snohomish", "spokane", "thurston", "whatcom", "yakima"))
+     
+colnames(wash)[6] <- "County"
+            
+wash_births <- full_join(wash, births, by = "County")
+
+wash_births$Births_cat <- cut(wash_births$Births, breaks = c(seq(0, 10000, by = 500)), labels=1:20)
+
+plot <- wash_births %>%
+  group_by(group) %>%
+  plot_ly(
+          x = ~long, 
+          y = ~lat, 
+          color = ~Births_cat, 
+          colors = c('#ffeda0','#f03b20'),
+          text = ~paste(paste(County), paste("Births:", Births), sep = "<br />"),
+          hoverinfo = 'text')  %>%
+  add_polygons(line = list(width = 0.4)) %>%
+  layout(
+    title = "2011~2015 Washington States Birth Rate<br />(Hover for more info)",
+    titlefont = list(size = 10),
+    xaxis = list(title = "", showgrid = FALSE,
+                 zeroline = FALSE, showticklabels = FALSE),
+    yaxis = list(title = "", showgrid = FALSE,
+                 zeroline = FALSE, showticklabels = FALSE)
+  )
+
+

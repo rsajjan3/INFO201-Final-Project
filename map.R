df3fb019c91eddfde3c438de82e5b5a5560935da
Hsien-Cheng Chou <jchou3@uw.edu>
2017-12-05 19:06:33 -0800

    map.R final

--- map.R
+++ map.R
@@ -6,41 +6,9 @@ library(plotly)
 library(ggplot2)
 library(tidyverse)
 library(maps)
-#natality.data <- read.csv('data.csv')
-## Read in data
-
-#wash <- map_data("county") %>%
-#  filter(region == 'washington')
-## Washington State county latitute and longtitude
-
-#colnames(wash)[6] <- "County"
-## Make the column name to County so that later can join with map.data
-
-#WA_births <- natality.data %>%   
-  ## Begin with the original dataset
-  
-#  filter(Year.Code == 2011) %>%
-  
-  ## Filter data by year values from slider widget
-  
-#  group_by(County) %>% summarise(births=sum(Births)) %>%
-  ## Group by county, and sum births for each county
-  
-#  filter(births >= 0, births <= 30000)
-## Filter data by min/max values for number of births
-
-#WA_births$County <- tolower(gsub(" County, WA","", WA_births$County))
-## lower case the county name
-
-#WA_births <- filter(WA_births, County %in% c("benton", "clark", "cowlitz", "king", "kitsap", "pierce", "skagit", "snohomish", "spokane", "thurston", "whatcom", "yakima"))
-## Delete the unidentity county
-
-#map.data <- full_join(wash, WA_births, by = "County")
 
 CreateMap <- function(map.data) {
-  #map.data$Births_cat <- cut(map.data$births, breaks = c(seq(0, 25000, by = 1)), labels=1:25000)
   map.data$Births_cat <- gsub("NA", "0", map.data$Births_cat)
-  View(map.data)
   plot <- map.data %>%
     group_by(group) %>%
     plot_ly(
@@ -48,7 +16,7 @@ CreateMap <- function(map.data) {
       y = ~lat, 
       color = ~Births_cat, 
       colors = c('#ffeda0','#f03b20'),
-      text = ~paste(paste(County), paste("Births:", births), sep = "<br />")
+      text = ~paste(County, paste("Births: ", births), sep = "<br />")
       ) %>%
     add_polygons(line = list(width = 0.4)) %>%
     add_polygons(
